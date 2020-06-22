57760a6deb25506a507ad68f61c5cc659aa7c107
legush <irinaseva83@gmail.com>
2020-06-22 12:42:09 +0500

    first commit

---
+++ 5_8/5_8.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int Fill(int m[10][10])
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			m[i][j] = rand();
+		}
+	}
+	return m[10][10];
+}
+
+int main()
+{
+
+	srand(time(0));
+	int Matrix[10][10];
+	Fill(Matrix);
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << Matrix[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+	int n, k;
+	for (int i = 0; i <= int(n - 1); i++)
+		for (int j = i + 1; j < n - i; j++)
+		{
+			k = Matrix[j][n - i - 1];
+			Matrix[j][n - i - 1] = Matrix[i][j];
+			Matrix[i][j] = Matrix[n - j - 1][i];
+			Matrix[n - j - 1][i] = Matrix[n - 1 - i][n - 1 - j];
+			Matrix[n - 1 - i][n - 1 - j] = k;
+		}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << Matrix[i][j] << "     ";
+			cout << "\n";
+		}
+		return 0;
+	}
+}
